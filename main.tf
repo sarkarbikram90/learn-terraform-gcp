75bbf73fcbbbd520172d36c3f1412eaa5fb8f1e0
bikram <sarkarbikram90@gmail.com>
2023-06-04 05:21:12 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("C:\\Users\\bikrams\\infraoptimize2023-62ae5bce0d52.json")
+
+  project = "infraoptimize2023"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
